9d07dc7caf449e504f7ecd221715193268f323d1
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-28 11:28:18 -0300

    ADD Envio de fotos e insercao na table view

--- PhotosAndCaptions/ViewController.swift
+++ PhotosAndCaptions/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var photos = [Photo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPhoto))
+    }
+    
+    //MARK: ImagePicker Methods
+    @objc func addNewPhoto() {
+        let picker = UIImagePickerController()
+
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString // generate a random name for the image
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        guard let jpegData = image.jpegData(compressionQuality: 0.8) else {
+            print("Cannot transform jpeg in data")
+            return
+        }
+        
+        try? jpegData.write(to: imagePath) // Save image in the url
+
+        let photo = Photo(image: imageName, description: "Photo")
+        photos.append(photo)
+        
+        tableView.reloadData()
+        
+        dismiss(animated: true)
     }
     
     //MARK: TableView Methods
@@ -32,9 +66,11 @@ class ViewController: UITableViewController {
         
         let imageURL = getDocumentsDirectory().appendingPathComponent(photo.image)
         photoCell.photoImageView.image = UIImage(contentsOfFile: imageURL.path)
+        photoCell.photoDescription.text = photo.description
         
         photoCell.imageView?.layer.borderWidth = 2
         photoCell.imageView?.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        
         
         return photoCell
     }
