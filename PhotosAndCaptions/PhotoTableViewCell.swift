436c6ff10bc3ee143d30162e2b68f09da1c0f71b
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-28 09:48:04 -0300

    ADD custom view cell e suas ligaçoes

---
+++ PhotosAndCaptions/PhotoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoTableViewCell.swift
+//  PhotosAndCaptions
+//
+//  Created by Joao Gabriel Dourado Cervo on 28/02/21.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var photoImageView: UIView!
+    @IBOutlet weak var photoDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
