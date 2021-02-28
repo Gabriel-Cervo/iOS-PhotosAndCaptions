c53a5ab0f07d89f4bd9fcc6c71f75fb9a976b397
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-28 16:14:40 -0300

    ADD visualização da imagem nos detalhes

--- PhotosAndCaptions/DetailViewController.swift
+++ PhotosAndCaptions/DetailViewController.swift
@@ -14,19 +14,37 @@ class DetailViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
-        
+                
         let imageURL = getDocumentsDirectory().appendingPathComponent(imageName!)
         
-        let imageView = UIImageView(image: UIImage(contentsOfFile: imageURL.path))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
-        imageView.layer.cornerRadius = 5
         
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        imageView.image = UIImage(contentsOfFile: imageURL.path)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        imageView.layer.cornerRadius = view.frame.size.width / 2
         view.addSubview(imageView)
+        
+        let labelView = UILabel()
+        labelView.text = imageDescription
+        labelView.font = .systemFont(ofSize: 16)
+        labelView.tintColor = .black
+        view.addSubview(labelView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 100),
+            imageView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+            imageView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.3),
+            imageView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: 100),
+            
+//            labelView.topAnchor.constraint(equalTo: imageView.bottomAnchor),
+        ])
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        print(imageDescription!)
     }
 }
